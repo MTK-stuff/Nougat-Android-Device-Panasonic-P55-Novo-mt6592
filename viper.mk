c179ccb399a5a5a96c76aa779b4a8993dc6edb9c
YJ_Shivom <yashpaljoshi76@gmail.com>
2017-11-18 19:17:02 +0530

    For viper

--- viper.mk
+++ viper.mk
@@ -8,7 +8,7 @@ VENDOR_PATH := vendor/Panasonic/P55Novo
 PRODUCT_RELEASE_NAME := P55Novo
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -19,7 +19,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := P55Novo
-PRODUCT_NAME := dot_P55Novo
+PRODUCT_NAME := viper_P55Novo
 PRODUCT_BRAND := Panasonic
 PRODUCT_MODEL := Panasonic P55 Novo by Shivom Techdroid
 PRODUCT_MANUFACTURER := P55Novo
